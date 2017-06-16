8f89ca947ec8fbe72ef3a1b977605b0d0d5f85d8
Rehan Manzoor <caprichano@gmail.com>
2017-06-17 01:12:48 +0400

    Updated documentation for config/config.py

--- config/config.py
+++ config/config.py
@@ -3,11 +3,46 @@ import os
 
 from base.support.map import Map
 
+r""" Provides Support for reading JSON based configuration files.
+
+This module helps in reading JSON based configuration files.
+
+
+e.g.
+
+config.json
+
+{
+    database:{
+        username: 'root',
+        pass: 'root'
+    }
+}
+
+You can get the username like
+
+Config.get('database.username')
+
+
+This module expose 3 static methods, Out of which use of only two is recommended.
+
+    get             get reads the json config contents recursively and gets the value using . notation path.
+    get_value       get value takes two parameters dictionary object and path to the final key.
+
+"""
+
 
 class Config(object):
     @staticmethod
     def get(path, default=None, config_file='/config/config.json'):
-
+        '''
+        Read the contents of the Configuration file and return the value
+        based on . notation path.
+        :param path:
+        :param default:
+        :param config_file:
+        :return:
+        '''
         file_contents = Config.get_contents(config_file)
 
         if file_contents is None:
@@ -17,7 +52,13 @@ class Config(object):
 
     @staticmethod
     def get_value(dict_object, path, default):
-
+        '''
+        Get value from dictionary based on . notation path.
+        :param dict_object:
+        :param path:
+        :param default:
+        :return:
+        '''
         try:
             index, key = path.split('.', 1)
         except:
@@ -38,6 +79,11 @@ class Config(object):
 
     @staticmethod
     def get_contents(config_file=None):
+        '''
+        Get contents of the file and convert these to json.
+        :param config_file:
+        :return:
+        '''
         file_contents = None
 
         with open(os.path.join(os.getcwd()) + config_file) as reader:
